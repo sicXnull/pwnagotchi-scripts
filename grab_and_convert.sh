09673000712bb5737725115acae5d6709b8e1a7c
sicxnull <cvanhauterjr@gmail.com>
2022-10-06 17:26:37 -0700

    initial commit

---
+++ grab_and_convert.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Author: sic
+INSTALL_LOCATION="$(pwd)"
+
+echo "*** MUST BE RUN AS ROOT ***"
+
+mkdir $INSTALL_LOCATION/handshakes/pcap
+mkdir $INSTALL_LOCATION/handshakes/hash
+ssh-keygen -t rsa
+ssh-copy-id root@10.0.0.2
+scp -r ~/.ssh/id_rsa.pub root@10.0.0.2:/home/pi/handshakes/*.pcap $INSTALL_LOCATION/handshakes/pcap
+scp -r ~/.ssh/id_rsa.pub root@10.0.0.2:/root/handshakes/*.pcap $INSTALL_LOCATION/handshakes/pcap
+rm $INSTALL_LOCATION/handshakes/pcap/*.pub
+
+cd $INSTALL_LOCATION/handshakes/pcap
+hcxpcapngtool -o hash.hc22000 -E wordlist.txt *
+cp hash.hc22000 wordlist.txt $INSTALL_LOCATION/handshakes/hash
+chmod -R a+rwx $INSTALL_LOCATION/*
