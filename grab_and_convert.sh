93024cc47b43e0c238c825e9c9b6c2366ba85e74
sicxnull <cvanhauterjr@gmail.com>
2022-10-08 13:26:26 -0700

    working

--- grab_and_convert.sh
+++ grab_and_convert.sh
@@ -14,6 +14,6 @@ scp -r ~/.ssh/id_rsa.pub root@10.0.0.2:/root/handshakes/*.pcap $INSTALL_LOCATION
 rm $INSTALL_LOCATION/handshakes/pcap/*.pub
 
 cd $INSTALL_LOCATION/handshakes/pcap
-hcxpcapngtool -o hash.hc22000 -E wordlist.txt *
-mv hash.hc22000 wordlist.txt $INSTALL_LOCATION/handshakes/hash
+hcxpcapngtool -o hash.hc22000 -E network-list.txt *
+mv hash.hc22000 $INSTALL_LOCATION/handshakes/hash
 chmod -R a+rwx $INSTALL_LOCATION/*
