8b60443500aff88e35d83371b637c2fba8da2e0c
sicXnull <cvanhauterjr@gmail.com>
2024-09-07 22:20:30 -0400

    make better

--- generate-attacks.py
+++ generate-attacks.py
@@ -1,207 +1,168 @@
-# Modified version of generate-hashcat-scripts.py which can be found here:
-# https://github.com/mtagius/pwnagotchi-tools
-# Big thanks to the original author
-
 import os
+import logging
 from dotenv import load_dotenv
 from random import randint
+from pathlib import Path
 
-load_dotenv()
-project_path = os.environ.get("PROJECT_PATH")
-hashcat_path = os.environ.get("HASHCAT_PATH")
-wordlist_path = os.environ.get("WORDLIST_PATH")
-
-# The FULL path to the 'pwnagotchi-scripts' folder in this repo
-fullProjectPath = f"{project_path}"
-
-# The FULL path to your hashcat 6.x.x install. Even if hashcat is added to your path,
-# there are problems saving and accessing the session files when running hashcat
-# commands while not in the hashcat folder, so the full path is needed
-fullHashcatPath = f"{hashcat_path}"
-
-# the FULL path to where your wordlists are saved
-fullWordListPath = f"{wordlist_path}"
-
-# This is the 'version' of the hashcat attacks in this file. This is
-# used to track the scripts used on the hc22000 files. If the
-# 'attacks' list below is manually updated this version can be incremented,
-# then if this script is run any hc22000 files with a 'waiting' status will
-# get new scripts generated for them.
-hashcatScriptVersion = "v1"
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 sessionScripts = []
 
-def generateHashcatScript(filename):
-    global hashcatScriptVersion
-    global sessionScripts
-    global fullProjectPath
-    global fullHashcatPath
-    global fullWordListPath
+# Load .env
+load_dotenv()
+project_path = Path(os.getenv("PROJECT_PATH", ""))
+hashcat_path = Path(os.getenv("HASHCAT_PATH", ""))
+wordlist_path = Path(os.getenv("WORDLIST_PATH", ""))
 
-    # if your GPU temp hits this temp hascat will stop
-    tempAbort = "--hwmon-temp-abort=100"
+if not all([project_path, hashcat_path, wordlist_path]):
+    raise ValueError("Environment variables for PROJECT_PATH, HASHCAT_PATH, or WORDLIST_PATH are missing!")
 
-    # determines how fast hashcat will run.
-    # 1 - slow, your computer will still be very usable
-    # 2 - default speed, you might notice some problems using your computer
-    # 3 - fast, your computer will not be usable while hashcat is running
-    # 4 - nightmare, only recommended for headless machines
-    workloadProfile = "-w 2"
+# Hashcat attack settings
+HASHCAT_SCRIPT_VERSION = "v1"
+TEMP_ABORT = "--hwmon-temp-abort=100"
+WORKLOAD_PROFILE = "-w 2"
+RULE_PATH = project_path / "hashcat/rules"
+WORD_NINJA_PATH = project_path / "hashcat"
+HASH_TYPE = "-m 22000"
+OUTPUT_FILE = project_path / "hashcat/hashcat-output.txt"
+POTFILE = project_path / "hashcat/hashcat-potfile.txt"
+OUTPUT_PATH = f'--potfile-path "{POTFILE}" -o "{OUTPUT_FILE}"'
 
-    outputPath = '--potfile-path "' + fullProjectPath + 'hashcat/hashcat-potfile.txt" -o "' + fullProjectPath + 'hashcat/hashcat-output.txt"'
-    rulePath = fullProjectPath + "hashcat/rules/"
-    wordNinjaPath = fullProjectPath + "hashcat"
+# Attacks list
+ATTACKS = [
+    ["-a 0", "known-wpa-passwords.txt", "quick-ssid.rule", "-S"],
+    ["-a 0", "known-wpa-passwords.txt", "unix-ninja-leetspeak.rule", "-S"],
+    ["-a 0", "known-wpa-passwords.txt", "rockyou-30000.rule", "-S"],
+    ["-a 0", "known-wpa-passwords.txt", "d3ad0ne.rule", "-S"],
+    ["-a 0", "nerdlist.txt", "quick-ssid.rule"],
+    ["-a 0", "nerdlist.txt", "unix-ninja-leetspeak.rule"],
+    ["-a 0", "nerdlist.txt", "rockyou-30000.rule"],
+    ["-a 0", "nerdlist.txt", "d3ad0ne.rule"],
+    ["-a 0", "bssid.rule"],
+    ["-a 0", "ssid-ninja.rule"],
+    ["-a 3", "MYWIFI?d?d?d?d"],
+    ["-a 3", "wifi?d?d?d?d"],
+    ["-a 3", "-1 !@$??#~%^&*^^ wifi?d?d?d?1"],
+    ["-a 3", "wifi?d?d?d?d?d"],
+    ["-a 3", "?d?d?d?dwifi"],
+    ["-a 3", "?d?d?d?d?dwifi"],
+    ["-a 3", "WIFI?d?d?d?d"],
+    ["-a 3", "-1 !@$??#~%^&*^^ WIFI?d?d?d?1"],
+    ["-a 3", "WIFI?d?d?d?d?d"],
+    ["-a 3", "?d?d?d?dWIFI"],
+    ["-a 3", "?d?d?d?d?dWIFI"],
+    ["-a 3", "?l?l?l?lwifi"],
+    ["-a 3", "-1 !@$??#~%^&*^^ ?l?l?l?lwifi?1"],
+    ["-a 3", "?l?l?l?l?lwifi"],
+    ["-a 3", "wifi?l?l?l?l"],
+    ["-a 3", "-1 !@$??#~%^&*^^ wifi?l?l?l?l?1"],
+    ["-a 3", "wifi?l?l?l?l?l"],
+    ["-a 3", "?u?l?l?lWifi"],
+    ["-a 3", "?u?l?l?l?lWifi"],
+    ["-a 3", "?u?u?u?uWIFI"],
+    ["-a 3", "-1 !@$??#~%^&*^^ ?u?u?u?uWIFI?1"],
+    ["-a 3", "?u?u?u?u?uWIFI"],
+    ["-a 3", "WIFI?u?u?u?u"],
+    ["-a 3", "-1 !@$??#~%^&*^^ WIFI?u?u?u?u?1"],
+    ["-a 3", "WIFI?u?u?u?u?u"],
+    ["-a 0", "NAMES.DIC", "names.rule"],
+    ["-a 0", "words_alpha.txt", "names.rule"],
+    ["-a 0", "4-digit-append.rule"],
+    ["-a 3", "?d?d?d?d?d?d?d?d"],
+    ["-a 6", "netgear-spectrum.txt", "?d?d?d"],
+    ["-a 6", "netgear-spectrum.txt", "?d"],
+    ["-a 6", "netgear-spectrum.txt", "?d?d"],
+    ["-a 0", "openwall.net-all.txt", "quick-ssid.rule"],
+    ["-a 0", "netgear-spectrum.txt", "quick-ssid.rule"],
+    ["-a 6", "words_alpha.txt", "?d"],
+    ["-a 6", "words_alpha.txt", "-1 !@$??#~%^&*^^ ?1"],
+    ["-a 6", "words_alpha.txt", "-1 !@$??#~%^&*^^ ?d?1"],
+    ["-a 6", "words_alpha.txt", "-1 !@$??#~%^&*^^ ?1?d"],
+    ["-a 6", "words_alpha.txt", "?d?d"],
+    ["-a 6", "words_alpha.txt", "-1 !@$??#~%^&*^^ ?d?d?1"],
+    ["-a 6", "words_alpha.txt", "?d?d?d"],
+    ["-a 3", "?l?l?l?l?l?l1!"],
+    ["-a 3", "?u?l?l?l?l?l1!"],
+    ["-a 3", "?u?u?u?u?u?u1!"],
+    ["-a 3", "?d?d?d?d?d?d?d?d?d"],
+    ["-a 0", "hashesorg2019"],
+    ["-a 0", "rockyou.txt", "quick-ssid.rule"],
+    ["-a 0", "NAMES.DIC", "rockyou-30000.rule"],
+    ["-a 0", "netgear-spectrum.txt", "unix-ninja-leetspeak.rule"],
+    ["-a 0", "Top1pt8Billion-WPA-probable-v2.txt"],
+    ["-a 0", "Top24Million-WPA-probable-v2.txt", "quick-ssid.rule"],
+    ["-a 0", "passphrases.txt", "passphrases.rule"],
+    ["-a 0", "Custom-WPA"],
+    ["-a 0", "Super-WPA"],
+    ["-a 3", "?h?h?h?h?h?h?h?h"],
+    ["-a 3", "?H?H?H?H?H?H?H?H"],
+    ["-a 3", "?d?d?d?d?d?d?d?d?d?d"]
+]
+
+# Generate the hashcat script
+def generate_hashcat_script(filename: str):
     ssid = filename.split("_")[0]
-    session = ""
-    hashType = ""
-    fileId = ""
-    hashPath = ""
-    hashType = "-m 22000"
-    fileId = filename.split(".hc22000")[0]
-    hashPath = '"' + fullProjectPath + 'handshakes/hash/' + filename + '"'
-    session = "--session " + fileId
+    file_id = filename.split(".hc22000")[0]
+    hash_path = project_path / "handshakes/hash" / filename
+    session = f"--session {file_id}_{randint(1000, 9999)}"
+    script_path = project_path / "hashcat/scripts" / f"{file_id}.sh"
 
-    attacks = [
-        ["-a 0", "known-wpa-passwords.txt", "quick-ssid.rule", "-S"],
-        # quick run of known passwords, the -S makes hashcat work better with small wordlists
-        ["-a 0", "known-wpa-passwords.txt", "unix-ninja-leetspeak.rule", "-S"],  # run over 3k leet rules
-        ["-a 0", "known-wpa-passwords.txt", "rockyou-30000.rule", "-S"],  # run of the best 30k rules for rockyou
-        ["-a 0", "known-wpa-passwords.txt", "d3ad0ne.rule", "-S"],  # over 34k rules
-        ["-a 0", "nerdlist.txt", "quick-ssid.rule", "-S"],
-        # nerdlist.txt is a small (< 500) list of "nerdy" passwords and references like "hacktheplanet"
-        ["-a 0", "nerdlist.txt", "unix-ninja-leetspeak.rule", "-S"],  # run over 3k leet rules
-        ["-a 0", "nerdlist.txt", "rockyou-30000.rule", "-S"],  # run of the best 30k rules for rockyou
-        ["-a 0", "nerdlist.txt", "d3ad0ne.rule", "-S"],  # over 34k rules
-        ["-a 0", "bssid.rule"],  # all variations of the BSSID of the network
-        ["-a 0", "ssid-ninja.rule"],
-        # uses wordNinjaGenerator.py to generate a wordlist from the ssid Ex: LuckyCoffeeWifi --> Lucky123!
-        ["-a 3", "MYWIFI?d?d?d?d"],  # all default passwords for MYWIFI (EE) routers
-        ["-a 3", "wifi?d?d?d?d"],  # for passwords like wifi1970
-        ["-a 3", "-1 !@$??#~%^&*^^ wifi?d?d?d?1"],
-        # for passwords like wifi123!, the charset looks weird because windows cmd chars must be escaped
-        ["-a 3", "wifi?d?d?d?d?d"],  # for passwords like wifi12345
-        ["-a 3", "?d?d?d?dwifi"],  # for passwords like 1989wifi
-        ["-a 3", "?d?d?d?d?dwifi"],  # for passwords like 12345wifi
-        ["-a 3", "WIFI?d?d?d?d"],  # for passwords like WIFI2008
-        ["-a 3", "-1 !@$??#~%^&*^^ WIFI?d?d?d?1"],  # for passwords like WIFI343@
-        ["-a 3", "WIFI?d?d?d?d?d"],  # for passwords like WIFI12345
-        ["-a 3", "?d?d?d?dWIFI"],  # for passwords like 2006WIFI
-        ["-a 3", "?d?d?d?d?dWIFI"],  # for passwords like 12345WIFI
-        ["-a 3", "?l?l?l?lwifi"],  # for passwords like bookwifi
-        ["-a 3", "-1 !@$??#~%^&*^^ ?l?l?l?lwifi?1"],  # for passwords like pinkwifi!
-        ["-a 3", "?l?l?l?l?lwifi"],  # for passwords like trackwifi
-        ["-a 3", "wifi?l?l?l?l"],  # for passwords like wificook
-        ["-a 3", "-1 !@$??#~%^&*^^ wifi?l?l?l?l?1"],  # for passwords like wificafe$
-        ["-a 3", "wifi?l?l?l?l?l"],  # for passwords like wififrogs
-        ["-a 3", "?u?l?l?lWifi"],  # for passwords like CafeWifi
-        ["-a 3", "?u?l?l?l?lWifi"],  # for passwords like MarioWifi
-        ["-a 3", "?u?u?u?uWIFI"],  # for passwords like CAFEWIFI
-        ["-a 3", "-1 !@$??#~%^&*^^ ?u?u?u?uWIFI?1"],  # for passwords like MECHWIFI*
-        ["-a 3", "?u?u?u?u?uWIFI"],  # for passwords like BULLSWIFI
-        ["-a 3", "WIFI?u?u?u?u"],  # for passwords like WIFISHOE
-        ["-a 3", "-1 !@$??#~%^&*^^ WIFI?u?u?u?u?1"],  # for passwords like WIFIBOAT!
-        ["-a 3", "WIFI?u?u?u?u?u"],  # for passwords like WIFICOACH
-        ["-a 0", "NAMES.DIC", "names.rule"],  # for passwords like lukeswifi
-        ["-a 0", "words_alpha.txt", "names.rule"],  # for passwords like pizzawifi
-        ["-a 0", "4-digit-append.rule"],
-        # uses wordNinjaGenerator.py to append all 1-4 digit combinations to ssid words Ex: MyCafeWifi --> CafeWifi2020
-        ["-a 3", "?d?d?d?d?d?d?d?d"],  # all 8 digit number combos
-        ["-a 6", "netgear-spectrum.txt", "?d?d?d"],
-        # MANY netgear routers have a default password that is a word + word + 1-3 digits, if I could only run 1 attack this is the one I would run
-        ["-a 6", "netgear-spectrum.txt", "?d"],
-        # MANY netgear routers have a default password that is a word + word + 1-3 digits
-        ["-a 6", "netgear-spectrum.txt", "?d?d"],
-        # MANY netgear routers have a default password that is a word + word + 1-3 digits
-        ["-a 0", "openwall.net-all.txt", "quick-ssid.rule"],  # openwall is a popular wordlist
-        ["-a 0", "netgear-spectrum.txt", "quick-ssid.rule"],  # for passwords like breezyapplewifi
-        ["-a 6", "words_alpha.txt", "?d"],  # for passwords like seashell1
-        ["-a 6", "words_alpha.txt", "-1 !@$??#~%^&*^^ ?1"],  # for passwords like seashell$
-        ["-a 6", "words_alpha.txt", "-1 !@$??#~%^&*^^ ?d?1"],  # for passwords like seashell1!
-        ["-a 6", "words_alpha.txt", "-1 !@$??#~%^&*^^ ?1?d"],  # for passwords like seashell!0
-        ["-a 6", "words_alpha.txt", "?d?d"],  # for passwords like seashell69
-        ["-a 6", "words_alpha.txt", "-1 !@$??#~%^&*^^ ?d?d?1"],  # for passwords like seashell92@
-        ["-a 6", "words_alpha.txt", "?d?d?d"],  # for passwords like seashell123
-        ["-a 3", "?l?l?l?l?l?l1!"],  # Ex: slyfox1!
-        ["-a 3", "?u?l?l?l?l?l1!"],  # Ex: Slyfox1!
-        ["-a 3", "?u?u?u?u?u?u1!"],  # Ex: SLYFOX1!
-        ["-a 3", "?d?d?d?d?d?d?d?d?d"],  # all 9 digit number combos
-        ["-a 0", "hashesorg2019"],  # hashesorg2019 is a popular wordlist
-        ["-a 0", "rockyou.txt", "quick-ssid.rule"],
-        # rockyou is a classic wordlist, quick-ssid.rule has rules made for wifi cracking, it's worth noting that rockyou comes from a database dump and online account passwords are often different from wifi passwords
-        ["-a 0", "NAMES.DIC", "rockyou-30000.rule"],  # for passwords like j0sh2009
-        ["-a 0", "netgear-spectrum.txt", "unix-ninja-leetspeak.rule"],  # for passwords like br33zyappl3
-        ["-a 0", "Top1pt8Billion-WPA-probable-v2.txt"],
-        # Top1pt8Billion-WPA-probable-v2.txt is a popular wordlist, it claims to be 'WPA probable' but really they just cut out all passwords less then 8 chars, that doesn't make it wifi probable
-        ["-a 0", "Top24Million-WPA-probable-v2.txt", "quick-ssid.rule"],
-        # Top24Million-WPA-probable-v2.txt is a popular wordlist, it claims to be 'WPA probable' but really they just cut out all passwords less then 8 chars, that doesn't make it wifi probable
-        ["-a 0", "passphrases.txt", "passphrases.rule"],
-        # from the passphrases repo, this SHOULD help with passwords like 'youshallnotpass'
-        ["-a 0", "Custom-WPA"],  # Custom-WPA is a popular wordlist
-        ["-a 0", "Super-WPA"],  # Super-WPA is a popular wordlist
-        ["-a 3", "?h?h?h?h?h?h?h?h"],  # MANY router default passwords are 8 hex chars (0-9,a-f)
-        ["-a 3", "?H?H?H?H?H?H?H?H"],  # MANY router default passwords are 8 hex chars (0-9,A-F)
-        ["-a 3", "?d?d?d?d?d?d?d?d?d?d"]
-        # this is, by far, the longest attack, but is covers ALL 10 digit combos which includes ALL US phone numbers
-    ]
+    logging.info(f"Creating script: {script_path}")
+    with script_path.open("w") as script_file:
+        script_file.write(f"# {HASHCAT_SCRIPT_VERSION}\n")
+        script_file.write(f'cd "{hashcat_path}"\n')
 
-    f = open(f"./hashcat/scripts/{fileId}.sh", "w")
-    script = ":: " + hashcatScriptVersion + "\n"
-    script += 'cd '
-    if fullProjectPath[0] != fullHashcatPath[0]:
-        # if the cd command needs the /d argument to change drives
-        script += '/d '
-    script += '"' + fullHashcatPath + '"\n'
-    for attack in attacks:
-        hashcatCommand = "./hashcat.bin "
-        hashcatCommand += attack[0] + " "
-        hashcatCommand += hashType + " "
-        hashcatCommand += session + "_" + str(randint(1000, 9999)) + " "
-        hashcatCommand += tempAbort + " "
-        hashcatCommand += workloadProfile + " "
-        hashcatCommand += outputPath + " "
-        hashcatCommand += hashPath + " "
-        if ("0" in attack[0]):
-            #Comment out for now. Will revisit later
-            if ("bssid.rule" in attack[1]):
-                hashcatCommand += '-r "' + rulePath + attack[1] + '"'
-                '''elif (("ssid-ninja.rule" in attack[1]) or ("4-digit-append.rule" in attack[1])):
-                hashcatCommand = 'python "' + wordNinjaPath + '/wordNinjaGenerator.py" ' + ssid + ' | ' + hashcatCommand
-                hashcatCommand += '-r "' + rulePath + attack[1] + '''
-            else:
-                if (len(attack) > 3 and "-S" in attack[3]):
-                    hashcatCommand += "-S "
-                hashcatCommand += '"' + fullWordListPath + attack[1] + '"'
-                if (len(attack) > 2):
-                    hashcatCommand += ' -r "' + rulePath + attack[2] + '"'
-        elif ("1" in attack[0]):
-            hashcatCommand += '"' + fullWordListPath + attack[1] + '" '
-            hashcatCommand += '"' + fullWordListPath + attack[2] + '"'
-        elif ("3" in attack[0]):
-            hashcatCommand += attack[1]
-        elif ("6" in attack[0]):
-            hashcatCommand += '"' + fullWordListPath + attack[1] + '"'
-            hashcatCommand += " " + attack[2]
-        hashcatCommand += "\n"
-        script += hashcatCommand
-    f.write(script)
-    f.close()
+        for attack in ATTACKS:
+            command = build_attack_command(attack, hash_path, session, ssid)
+            script_file.write(f"{command}\n")
 
-    sessionScripts.append(f"{fileId}.sh")
+    sessionScripts.append(f"{file_id}.sh")
 
-def generateScriptsForHCs():
-    global hashcatScriptVersion
-    for folder in ["hash"]:
-        for filename in os.listdir("./handshakes/" + folder):
-                print("Attacks ready for " + filename)
-                generateHashcatScript(filename)
-def printLogo():
-    print('''
+# Build hashcat commands
+def build_attack_command(attack, hash_path, session, ssid):
+    command = f'./hashcat.bin {attack[0]} {HASH_TYPE} {session} {TEMP_ABORT} {WORKLOAD_PROFILE} {OUTPUT_PATH} "{hash_path}"'
 
+    if attack[0] == "-a 0":  # Dictionary attack
+        command = handle_dict_attack(attack, command, ssid)
+    elif attack[0] == "-a 3":  # Mask attack
+        command += f" {attack[1]}"
+    elif attack[0] == "-a 6":  # Hybrid attack
+        command += f' "{wordlist_path / attack[1]}" {attack[2]}'
+
+    return command
+
+# Handle dictionary attacks
+def handle_dict_attack(attack, command, ssid):
+    if "bssid.rule" in attack[1]:
+        return f'{command} -r "{RULE_PATH / attack[1]}"'
+    if "ssid-ninja.rule" in attack[1]:
+        return f'python "{WORD_NINJA_PATH / "wordNinjaGenerator.py"}" {ssid} | {command} -r "{RULE_PATH / attack[1]}"'
+    command += f' "{wordlist_path / attack[1]}"'
+    if len(attack) > 2:
+        command += f' -r "{RULE_PATH / attack[2]}"'
+    return command
+
+# Generate scripts for handshakes
+def generate_scripts_for_hcs():
+    handshake_dir = project_path / "handshakes/hash"
+    if not handshake_dir.exists():
+        logging.error(f"Handshake directory {handshake_dir} not found!")
+        return
+
+    for filename in handshake_dir.iterdir():
+        if filename.suffix == ".hc22000":
+            logging.info(f"Generating attacks for {filename.name}")
+            generate_hashcat_script(filename.name)
+
+def print_logo():
+    logo = '''
 ██████  ███████  █████  ██████  ██    ██     ██████      ██████  ██     ██ ███    ██ 
 ██   ██ ██      ██   ██ ██   ██  ██  ██           ██     ██   ██ ██     ██ ████   ██ 
 ██████  █████   ███████ ██   ██   ████        █████      ██████  ██  █  ██ ██ ██  ██ 
 ██   ██ ██      ██   ██ ██   ██    ██        ██          ██      ██ ███ ██ ██  ██ ██ 
 ██   ██ ███████ ██   ██ ██████     ██        ███████     ██       ███ ███  ██   ████ 
-                                                                              
-    ''')
-printLogo()
-generateScriptsForHCs()
+    '''
+    print(logo)
+
+if __name__ == "__main__":
+    print_logo()
+    generate_scripts_for_hcs()
--- grab_and_convert.sh
+++ grab_and_convert.sh
@@ -1,37 +1,77 @@
 #!/bin/sh
 # Author: sic
+
+# Define variables
 INSTALL_LOCATION="$(pwd)"
-SSHKEY=$INSTALL_LOCATION/ssh/id_rsa
-HASHCAT_LOCATION=$INSTALL_LOCATION/hashcat/hashcat
-WORDLIST_LOCATION=$INSTALL_LOCATION/wordlists
+SSHKEY="$INSTALL_LOCATION/ssh/id_rsa"
+SSHKEY_PUB="$INSTALL_LOCATION/ssh/id_rsa.pub"
+HASHCAT_LOCATION="$INSTALL_LOCATION/hashcat/hashcat"
+WORDLIST_LOCATION="$INSTALL_LOCATION/wordlists"
+REMOTE_HOST="10.0.0.2"
+REMOTE_DIR="/root/handshakes"
+LOCAL_HANDSHAKES_DIR="$INSTALL_LOCATION/handshakes/pcap"
 
-
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
+# Create necessary directories
+mkdir -p "$INSTALL_LOCATION/handshakes/pcap" \
+         "$INSTALL_LOCATION/handshakes/hash" \
+         "$INSTALL_LOCATION/hashcat/scripts" \
+         "$INSTALL_LOCATION/ssh"
+         
 cat <<EOT > .env
 PROJECT_PATH="$INSTALL_LOCATION/"
 HASHCAT_PATH="$HASHCAT_LOCATION"
 WORDLIST_PATH="$WORDLIST_LOCATION/"
 EOT
 
-mkdir -p $INSTALL_LOCATION/handshakes $INSTALL_LOCATION/handshakes/pcap $INSTALL_LOCATION/handshakes/hash $INSTALL_LOCATION/hashcat/scripts $INSTALL_LOCATION/ssh
-
-if [ ! -f $SSHKEY ]; then
-echo "*** Generating SSH Key ***"
-ssh-keygen -t rsa -f $INSTALL_LOCATION/ssh/id_rsa
-ssh-copy-id -i $INSTALL_LOCATION/ssh/id_rsa root@10.0.0.2
+# Generate SSH key if not already present
+if [ ! -f "$SSHKEY" ]; then
+  echo "*** Generating SSH Key ***"
+  ssh-keygen -t rsa -f "$SSHKEY" -N ""
+  chmod 600 "$SSHKEY"
+  chmod 644 "$SSHKEY_PUB"
+  ssh-copy-id -i "$SSHKEY_PUB" root@"$REMOTE_HOST"
 fi
-scp -r $INSTALL_LOCATION/ssh/id_rsa root@10.0.0.2:/home/pi/handshakes/*.pcap $INSTALL_LOCATION/handshakes/pcap
-scp -r $INSTALL_LOCATION/ssh/id_rsa root@10.0.0.2:/root/handshakes/*.pcap $INSTALL_LOCATION/handshakes/pcap
-rm $INSTALL_LOCATION/handshakes/pcap/*.pub
-cd $INSTALL_LOCATION/handshakes/pcap
-echo "*** Convert to hc22000 ***"
-hcxpcapngtool -o hash.hc22000 -E $INSTALL_LOCATION/network-list.txt *
-mv hash.hc22000 $INSTALL_LOCATION/handshakes/hash
-chmod -R a+rwx $INSTALL_LOCATION/*
-cd $INSTALL_LOCATION
+
+# Set proper permissions on the SSH key
+chmod 600 "$SSHKEY"
+chmod 644 "$SSHKEY_PUB"
+
+# Verify SSH key has been copied correctly
+echo "*** Verifying SSH Key ***"
+ssh -i "$SSHKEY" root@"$REMOTE_HOST" exit
+if [ $? -ne 0 ]; then
+  echo "SSH key authentication failed. Please check the SSH key and remote server settings."
+  exit 1
+fi
+
+# Copy pcap files from remote host
+echo "*** Copying pcap files from remote host ***"
+scp -i "$SSHKEY" root@"$REMOTE_HOST":"$REMOTE_DIR/*.pcap" "$LOCAL_HANDSHAKES_DIR"
+
+# Remove any public key files if present
+rm -f "$LOCAL_HANDSHAKES_DIR/*.pub"
+
+# Convert pcap files to hc22000 format
+echo "*** Converting to hc22000 format ***"
+cd "$LOCAL_HANDSHAKES_DIR" || {
+  echo "Failed to change directory to $LOCAL_HANDSHAKES_DIR"
+  exit 1
+}
+hcxpcapngtool -o hash.hc22000 -E "$INSTALL_LOCATION/network-list.txt" *.pcap
+mv hash.hc22000 "$INSTALL_LOCATION/handshakes/hash"
+
+# Set permissions
+chmod -R a+rwx "$INSTALL_LOCATION"
+
+# Generate Hashcat attacks
 echo "*** Generating Hashcat Attacks ***"
-python3 generate-attacks.py
+cd "$INSTALL_LOCATION" || {
+  echo "Failed to change directory to $INSTALL_LOCATION"
+  exit 1
+}
+python3 generate-attacks.py || {
+  echo "Failed to generate Hashcat attacks"
+  exit 1
+}
+
+echo "*** Script completed successfully ***"
